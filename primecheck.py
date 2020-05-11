07e383d8c2e196963622544fbd3d9545cb586de1
sujoy98 <32317613+sujoy98@users.noreply.github.com>
2020-05-11 19:00:33 +0530

    Add files via upload

--- primecheck.py
+++ primecheck.py
@@ -3,7 +3,16 @@ for i in range(2, num):
     if num % i == 0:
         print("NOT PRIME")
         break
+else: # using FOR-ELSE
+    print("PRIME")
+
+'''if we dont want to use for else then
+
+num = int(input("Enter a number to be checked :"))
+for i in range(2, num):
+    if num % i == 0:
+        print("NOT PRIME")
+        break
     else:
         print("PRIME")
-        break
-        
+        break '''
